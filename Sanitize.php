2f79d8e7e69963bada3bc4d59e7c79a2ba7053bd
Adam Bowen <adamnbowen@gmail.com>
2011-10-11 23:17:05 -0400

    Add Docblock comments.  Probably mostly unnecessary.

--- Sanitize.php
+++ Sanitize.php
@@ -1,7 +1,26 @@
 <?php
+/**
+ * Sanitize A class for sanitizing arrays and objects, with the additional
+ * feature of returning null for nonexistent properties.
+ * 
+ * @author Adam Bowen <adamnbowen@gmail.com>
+ * @license http://dbad-license.org/license DBAD license
+ */
 class Sanitize {
+  /**
+   * _sanitizedArray the sanitized result of the array/object passed to the
+   * constructor
+   * 
+   * @var array
+   */
   private $_sanitizedArray = array();
 
+  /**
+   * __construct Sanitize the keys and values of the $uncleanArray.
+   * 
+   * @param mixed $uncleanArray 
+   * @return void
+   */
   public function __construct($uncleanArray)
   {
     foreach ($uncleanArray as $key => $value) {
@@ -9,6 +28,16 @@ class Sanitize {
     }
   }
 
+  /**
+   * _sanitize Sanitize the given $input.
+   *
+   * Sends all inputs to _fixIncompleteObject() to ensure there are no broken
+   * objects, then if $input is an object or an array, it cleans $input
+   * recursively.  If $input is a string, it is simply cleaned and returned.
+   * 
+   * @param mixed $input 
+   * @return mixed Either a simple cleaned string or a cleaned array.
+   */
   private function _sanitize($input)
   {
     $input = $this->_fixIncompleteObject($input);
@@ -39,6 +68,14 @@ class Sanitize {
     return $input;
   }
 
+  /**
+   * __get If a nonexistent property of a Sanitize object is called, this
+   * function checks to see if the property corresponds to a key of
+   * $this->_sanitizedArray, and returns that, otherwise it returns null.
+   * 
+   * @param mixed $key 
+   * @return void
+   */
   public function __get($key)
   {
     if (array_key_exists($key, $this->_sanitizedArray)
