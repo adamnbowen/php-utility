5c0ffb91df651c2c4e0625b3db24046d82f8787c
Adam Bowen <adamnbowen@gmail.com>
2011-10-11 22:58:15 -0400

    Whitespace change---2 space tab width

--- Sanitize.php
+++ Sanitize.php
@@ -1,52 +1,52 @@
 <?php
 class Sanitize {
-    private $_sanitizedArray = array();
+  private $_sanitizedArray = array();
 
-    public function __construct($uncleanArray)
-    {
-        foreach ($uncleanArray as $key => $value) {
-            $this->_sanitizedArray[$this->_sanitize($key)] = $this->_sanitize($value);
-        }
+  public function __construct($uncleanArray)
+  {
+    foreach ($uncleanArray as $key => $value) {
+      $this->_sanitizedArray[$this->_sanitize($key)] = $this->_sanitize($value);
     }
+  }
 
-    private function _sanitize($input)
-    {
-        $input = $this->_fixIncompleteObject($input);
+  private function _sanitize($input)
+  {
+    $input = $this->_fixIncompleteObject($input);
 
-        if (is_array($input) || is_object($input)) {
-            $output = array();
-            foreach ($input as $key => $value){
-                $output[$key] = $this->_sanitize($value);
-            }
-            return $output;
-        }
-        return stripslashes(htmlentities(strip_tags(trim($input))));
+    if (is_array($input) || is_object($input)) {
+      $output = array();
+      foreach ($input as $key => $value){
+        $output[$key] = $this->_sanitize($value);
+      }
+      return $output;
     }
+    return stripslashes(htmlentities(strip_tags(trim($input))));
+  }
 
-    /**
-     * _fixIncompleteObject repairs an object if it is incomplete.
-     *
-     * Removes the __PHP_Incomplete_Class crap from the object, so is_object()
-     * will correctly identify $input as an object
-     *
-     * @param object $object The "broken" object
-     * @return object The "fixed" object
-     */
-    private function _fixIncompleteObject($input) {
-        if (!is_object($input) && gettype($input) == 'object') {
-            return unserialize(serialize($input));
-        }
-        return $input;
+  /**
+   * _fixIncompleteObject repairs an object if it is incomplete.
+   *
+   * Removes the __PHP_Incomplete_Class crap from the object, so is_object()
+   * will correctly identify $input as an object
+   *
+   * @param object $object The "broken" object
+   * @return object The "fixed" object
+   */
+  private function _fixIncompleteObject($input) {
+    if (!is_object($input) && gettype($input) == 'object') {
+      return unserialize(serialize($input));
     }
+    return $input;
+  }
 
-    public function __get($key)
-    {
-        if (array_key_exists($key, $this->_sanitizedArray)
-            && !empty($this->_sanitizedArray[$key])
-        ) {
-            return $this->_sanitizedArray[$key];
-        } else {
-            return null;
-        }
+  public function __get($key)
+  {
+    if (array_key_exists($key, $this->_sanitizedArray)
+      && !empty($this->_sanitizedArray[$key])
+    ) {
+      return $this->_sanitizedArray[$key];
+    } else {
+      return null;
     }
+  }
 }
